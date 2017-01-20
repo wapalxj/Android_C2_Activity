faf699ad6fb71e51a3e099087507402febf72d99
wapalxj <waplaxj@163.com>
2017-01-20 22:19:26 +0800

    170120

--- Activity/c2_2_21_excise/src/main/java/com/vero/c2_2_21_excise/PhoneCall.java
+++ Activity/c2_2_21_excise/src/main/java/com/vero/c2_2_21_excise/PhoneCall.java
@@ -30,25 +30,4 @@ public class PhoneCall extends ActionBarActivity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_phon_call, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- Activity/c2_2_21_excise2_intent/src/main/java/com/book/vero/c2_2_21_excise2_intent/MainActivity.java
+++ Activity/c2_2_21_excise2_intent/src/main/java/com/book/vero/c2_2_21_excise2_intent/MainActivity.java
@@ -4,14 +4,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button view;
     private Button call;
+    private Button dial;
     private Button sms;
     private Button share;
     private Button start;
@@ -26,9 +25,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         Intent i;
         switch (v.getId()){
-            case R.id.view:
-                i=new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("http://www.baidu.com"));
+            case R.id.dial:
+                i=new Intent(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:18468050252"));
                 startActivity(i);
                 break;
             case R.id.call:
@@ -64,12 +63,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     public void init(){
         view=(Button)findViewById(R.id.view);
+        dial=(Button)findViewById(R.id.dial);
         call=(Button)findViewById(R.id.call);
         sms=(Button)findViewById(R.id.sms);
         share=(Button)findViewById(R.id.share);
         start=(Button)findViewById(R.id.startActivity);
         goReader=(Button)findViewById(R.id.goReader);
         view.setOnClickListener(this);
+        dial.setOnClickListener(this);
         call.setOnClickListener(this);
         sms.setOnClickListener(this);
         share.setOnClickListener(this);
